22177fc8a70c1c319f6b2e6aeb16c3e90d4ec0f4
marybratt <mbratt56@gmail.com>
2018-12-11 09:47:25 -0800

    another try to add

---
+++ helloWorld.java
@@ -0,0 +1,15 @@
+package hellowWorld;
+
+public class helloWorld {
+	/* We will be practicing moving things to git and then 
+	 * moving changes to git.
+	 */
+
+	public static void main(String[] args) {
+		// This will print out Hello Everyone
+		System.out.println("Hello Everyone!");
+
+	}
+
+}
+"# homework1"
